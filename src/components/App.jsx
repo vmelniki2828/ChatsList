b9d73a909e99a6949ac9aaab0a78cc60400eeaea
Vlad Melnik <vmelniki2828@gmail.com>
2024-06-26 14:47:12 +0300

    first edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
+import React, { useState } from 'react';
+import chatData from './results.json';
+
+const ChatRecord = ({ record, index }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const points = [25, 10, 20, 10, 35];
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <button onClick={toggleOpen}>
+        {isOpen ? 'Скрыть' : 'Показать'} чат {index + 1}
+      </button>
+      {isOpen && (
+        <div>
+          <p>{record.chat}</p>
+          <div style={{ display: 'flex' }}>
+            {record.ans.map((ans, index) => (
+              <span key={index}>
+                {ans ? 'true' : 'false'}
+                {index !== record.ans.length - 1 && <span>, </span>}
+              </span>
+            ))}
+          </div>
+          {(() => {
+            let totalPoints = 0;
+            for (let i = 0; i < record.ans.length; i++) {
+              if (!record.ans[i]) {
+                totalPoints += points[i];
+              }
+            }
+            return <p>{totalPoints}</p>;
+          })()}
+        </div>
+      )}
+    </div>
+  );
+};
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      {chatData.map((record, index) => (
+        <ChatRecord key={index} record={record} index={index} />
+      ))}
     </div>
   );
 };
